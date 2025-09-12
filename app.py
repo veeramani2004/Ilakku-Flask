42f1854ca8d5cad3084829e864367cdc01ec9059
veeramani2004 <veeramanipalanisamy642004@gmail.com>
2025-09-12 07:14:55 +0530

    error messege created for token

--- app.py
+++ app.py
@@ -18,22 +18,22 @@ db.init_app(app)  # Call
 jwt.init_app(app)
 
 
-# @jwt.unauthorized_loader
-# def _unauth(e):
-#     return {"error": "missing/invalid token"}, 401
+@jwt.unauthorized_loader
+def _unauth(e):
+    return {"error": "missing/invalid token"}, 401
 
 
-# @jwt.expired_token_loader
-# def _expired(h, p):
-#     return ({"error": "token expired"}), 401
+@jwt.expired_token_loader
+def _expired(h, p):
+    return ({"error": "token expired"}), 401
 
 
-# with app.app_context():
-#     try:
-#         result = db.session.execute(text("SELECT 1")).fetchall()
-#         print("Connection successful:", result)
-#     except Exception as e:
-#         print("Error connecting to the database:", e)
+with app.app_context():
+    try:
+        result = db.session.execute(text("SELECT 1")).fetchall()
+        print("Connection successful:", result)
+    except Exception as e:
+        print("Error connecting to the database:", e)
 
 
 @app.get("/")
